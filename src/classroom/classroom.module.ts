df27103930813ae717494265cfc632a2bb38f3f3
Ethan <github.xdgcd@passmail.net>
2025-02-06 14:25:07 +0100

    ✨Define auth guard for classroom and reservation

--- src/classroom/classroom.module.ts
+++ src/classroom/classroom.module.ts
@@ -3,11 +3,11 @@ import { ClassroomService } from './classroom.service';
 import { ClassroomController } from './classroom.controller';
 import { ClassroomEntity } from './entities/classroom.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ClassroomEntity])],
+  imports: [TypeOrmModule.forFeature([ClassroomEntity]), UserModule],
   controllers: [ClassroomController],
   providers: [ClassroomService],
-  exports: [ClassroomService],
 })
 export class ClassroomModule {}
