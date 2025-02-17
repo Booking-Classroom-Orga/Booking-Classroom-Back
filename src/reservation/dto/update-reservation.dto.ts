47fa7111e54b2e5b944be84ad4650c725d4c719b
Ethan Frot <contact@ethanfrot.com>
2025-02-17 09:23:14 +0100

    🧑‍💻Add userId property to updateReservationDto

--- src/reservation/dto/update-reservation.dto.ts
+++ src/reservation/dto/update-reservation.dto.ts
@@ -5,4 +5,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class UpdateReservationDto extends PartialType(CreateReservationDto) {
   @ApiProperty()
   id: number;
+
+  @ApiProperty()
+  userId: number;
 }
