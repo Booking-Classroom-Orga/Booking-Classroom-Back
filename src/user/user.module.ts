510e7b7d64d1a3a3581a43a29a190f3cee35a4ff
Ethan Frot <contact@ethanfrot.com>
2025-02-18 16:12:14 +0100

    🐛Delete reservations of the user deleted

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserEntity } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReservationEntity } from '../reservation/entities/reservation.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, ReservationEntity])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
