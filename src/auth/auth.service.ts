352bd4adbfd4a692f60b09a42d3b5c088db4586d
Ethan Frot <contact@ethanfrot.com>
2025-02-18 10:53:55 +0100

    ✨Implement verificationCode by email for login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,18 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import { MailService } from '../mail/mail.service';
+import { VerifyCodeDto } from './dto/verify-code.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly mailService: MailService,
   ) {}
 
   async login(loginDto: LoginDto) {
@@ -23,6 +26,28 @@ export class AuthService {
     if (!isPasswordCorrect) {
       throw new UnauthorizedException('Invalid password');
     }
+
+    const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+    user.verificationCode = verificationCode;
+    await this.userService.update(user.id, user);
+
+    try {
+      await this.mailService.sendVerificationMail(user, verificationCode);
+    } catch (error) {
+      throw new InternalServerErrorException("The mail couldn't be send", error);
+    }
+
+    return { message: 'Verification code sent to your email' };
+  }
+
+  async verifyCode(verifyCodeDto: VerifyCodeDto) {
+    const user = await this.userService.findOneByEmail(verifyCodeDto.email);
+    if (!user || user.verificationCode !== verifyCodeDto.code) {
+      throw new UnauthorizedException('Invalid verification code');
+    }
+
+    user.verificationCode = null;
+    await this.userService.update(user.id, user);
 
     const payload = {
       id: user.id,
